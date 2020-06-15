31ca1e03b70f367a2bbe77703cb321dd987345bb
tungli <tun@mail.muni.cz>
2020-06-15 16:18:23 +0200

    minor update (test)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,17 @@
 using Findpeaks
 using Test
 
-gaussian(x,μ,σ) = 1/sqrt(2*π)/σ*exp(-((x-μ)^2)/2/σ^2)
-x = collect(range(1,stop=1000,length=1000))
-μ = rand(x)
-data = gaussian.(x,μ,rand(x)/3)
+@testset "Single Gaussian peak" begin
+    gaussian(x,μ,σ) = 1/sqrt(2*π)/σ*exp(-((x-μ)^2)/2/σ^2)
+    x = collect(range(1,stop=1000,length=1000))
+    μ = rand(x)
+    data = gaussian.(x,μ,rand(x)/3)
 
-p = findpeaks(data)
-@test length(p) == 1
-@test p[1] == μ
-p = findpeaks(data,x)
-@test length(p) == 1
-@test p[1] == μ
+    p = findpeaks(data)
+    @test length(p) == 1
+    @test p[1] == μ
+
+    p = findpeaks(data,x)
+    @test length(p) == 1
+    @test p[1] == μ
+end

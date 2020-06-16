cda748fe1bd986ad69c78b5a18d80307a78a6cbf
tungli <tun@mail.muni.cz>
2020-06-16 11:15:25 +0200

    prominence test

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,3 +15,29 @@ using Test
     @test length(p) == 1
     @test p[1] == μ
 end
+
+
+@testset "Prominence" begin
+    gaussian(x,μ,σ) = exp(-((x-μ)^2)/2/σ^2)
+
+    x = range(0., stop = 1., length = 1000) |> collect
+
+    positions = [1, 2, 3, 5, 7, 8]/10;
+    amps = [3, 7, 5, 5, 4, 5];
+    widths = [1, 3, 3, 4, 2, 3]/100;
+
+    base = 4 * cos.(2π * x);
+    signal = ( a * gaussian.(x, μ, σ) for (a, μ, σ) in zip(amps, positions, widths) )
+    data = sum(signal) + base
+
+    peaks = findpeaks(data, x, min_prom=4.)
+    
+    @test length(peaks) == 2
+
+    expected_peak_1 = argmin(abs.(x .- positions[2])) # global highest peak
+    expected_peak_2 = argmin(abs.(x .- positions[4])) # lowest peak
+
+    # check peaks are around expected - may be shifted because of background
+    @test abs(peaks[1] - expected_peak_1) < 20
+    @test abs(peaks[2] - expected_peak_2) < 20
+end
